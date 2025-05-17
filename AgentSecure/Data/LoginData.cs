be27d509ad728f1c6281521e253dd06ef705dbd1
MJSuttles <rolltiderolldad@gmail.com>
2025-05-17 12:29:59 -0500

    structured Data files

---
+++ AgentSecure/Data/LoginData.cs
@@ -0,0 +1,12 @@
+using AgentSecure.Models;
+
+namespace AgentSecure.Data
+{
+  public class LoginData
+  {
+    public static List<Login> Logins = new()
+    {
+      // Seed data
+    };
+  }
+}
---
+++ AgentSecure/Data/UserData.cs
@@ -0,0 +1,12 @@
+using AgentSecure.Models;
+
+namespace AgentSecure.Data
+{
+  public class UserData
+  {
+    public static List<User> Users = new()
+    {
+      // Seed data
+    };
+  }
+}
