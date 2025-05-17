0cc2fa9976cee8209c64bb2111777dde228cbe54
MJSuttles <rolltiderolldad@gmail.com>
2025-05-17 12:55:41 -0500

    structred Endpoints

---
+++ AgentSecure/Endpoints/VendorCategoryEnpoints.cs
@@ -0,0 +1,23 @@
+using System.Runtime.InteropServices;
+using AgentSecure.Interfaces;
+using AgentSecure.Models;
+
+namespace AgentSecure.Endpoint
+{
+  public static class VendorCategoryEndpoints
+  {
+    // The endpoint layer is responsible for handling HTTP requests.
+    // The endpoint layer will call the service layer to process business logic.
+    // The endpoint layer will return the data to the client.
+    // The endpoint layer is the entry point for the client to access the application.
+    // We must register this MapWeatherEndpoints method in the Program.cs file.
+    // You can click the reference to see where it is registered in the Program.cs file.
+
+    public static void MapVendorCategoryEndpoints(this IEndpointRouteBuilder routes)
+    {
+      var group = routes.MapGroup("/api/vendorcategories").WithTags(nameof(VendorCategory));
+
+      // API calls
+    }
+  }
+}
