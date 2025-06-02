554152cd7e2264e1ac0ed2c64f20fe3460f3bbcd
MJSuttles <rolltiderolldad@gmail.com>
2025-06-02 12:54:21 -0500

    coded tests for GetAllUsersAsync, GetUserByIdAsync, UpdateUserAsync, and DeleteUserAsync

--- AgentSecure/DTOs/UserProfileDto.cs
+++ AgentSecure/DTOs/UserProfileDto.cs
@@ -2,6 +2,7 @@ namespace AgentSecure.DTOs
 {
   public class UserProfileDto
   {
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
--- AgentSecure/DTOs/UserProfileUpdateDto.cs
+++ AgentSecure/DTOs/UserProfileUpdateDto.cs
@@ -2,6 +2,7 @@ namespace AgentSecure.DTOs
 {
   public class UserProfileUpdateDto
   {
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
